da6fa050f2ce064fc93f22eb30b1145d056a836a
Stav Barak <stav@lynx.md>
2019-05-05 15:48:59 +0300

    bootstrap nav example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MainPage from './components/MainPage';
+import ItemInfo from './components/ItemInfo';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    // <div className="App">
+    //   <MainPage />
+    // </div>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/:id" component={ ItemInfo } />
+          <Route path="/" component={ MainPage } />
+        </Switch>
+    </BrowserRouter> 
   );
 }
 
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const ItemCard = ( { id, name, price } ) => {
     return (
         <Card>
-            <Card.Header>
-                 <Card.Img src={`https://loremflickr.com/320/240?random=${id}`} alt="Card image" />
-            </Card.Header>
-            <Card.Body>
-                <Card.Title>
-                    { name }
-                </Card.Title>
-                <Card.Text>
-                    { price }
-                </Card.Text>
-            </Card.Body>       
+            <Link to={`${id}`} >
+                <Card.Header>
+                    <Card.Img src={`https://loremflickr.com/320/240?random=${id}`} alt="Card image" />
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>
+                        { name }
+                    </Card.Title>
+                    <Card.Text>
+                        { price }
+                    </Card.Text>
+                </Card.Body>    
+            </Link>   
         </Card>  
+       
     ) 
 }
 
