e5939fdde823c98bd9c3f55a17afb58b7422bbb2
Stav Barak <stav@lynx.md>
2019-05-05 15:12:06 +0300

    random photos

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,7 +3,10 @@ import Card from 'react-bootstrap/Card';
 
 const ItemCard = ( { id, name, price } ) => {
     return (
-        <Card key={ id }>
+        <Card>
+            <Card.Header>
+                 <Card.Img src={`https://loremflickr.com/320/240?random=${id}`} alt="Card image" />
+            </Card.Header>
             <Card.Body>
                 <Card.Title>
                     { name }
