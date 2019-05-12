ba769436e1eda1ae113edd0c9df950257d836a70
Stav Barak <stavbarak@gmail.com>
2019-05-12 22:37:24 +0300

    info

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -1,21 +1,22 @@
 import React, { Component }  from 'react';
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
-// import Button from 'react-bootstrap/Button';
-// import { getItems, getSpecificItem } from '../api';
+import Button from 'react-bootstrap/Button';
 
 class ItemInfo extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            currentItem: null
+            currentItem: null,
+            activeTabContent: null,
+            firstActive: true
         }
     }
 
     componentDidMount = () => {
         const { id } = this.props.match.params;
-        const FETCH_URL = 'http://www.mocky.io/v2/5ccf3f8f300000ce1652c4bc';
+        const FETCH_URL = 'http://www.mocky.io/v2/5cd864f4300000a22a74cda3';
         fetch(FETCH_URL, {
           method: 'GET'
         })
@@ -34,36 +35,50 @@ class ItemInfo extends Component {
           })
     }
 
-    render() {
-        const { currentItem } = this.state;
-        //console.log(this.state.currentItem)
-        return (
-            <Card>           
-            <Card.Header>
-                {currentItem ? <Card.Img src={ currentItem.image } alt="Card image" /> : ''}               
-            </Card.Header>
-            <Card.Body>
-                {currentItem ? <Card.Title>{ currentItem.name }</Card.Title> : ''}
+    switchTab(selectedKey) {
+        this.setState({
+            activeTabContent: selectedKey,
+            firstActive: false
+        })
+    }
 
-                {/* // bootstrap nav example - no data */}
-                <Nav justify variant="tabs" defaultActiveKey="/home">
-                    <Nav.Item>
-                        <Nav.Link href="/home">Stuff</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="link-1">More stuff</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="link-1">More</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="link-1">Even more</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </Card.Body>             
-        </Card>  
- 
-        ) 
+    render() {
+        const { currentItem, activeTabContent } = this.state;
+        if(currentItem!==null) {
+            return (
+                <Card className="item-info col-md-8">           
+                <Card.Header>
+                   <Card.Img src={ currentItem.image } alt="Card image" />         
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>{ currentItem.name }</Card.Title>
+                        <Nav fill defaultActiveKey="#" onSelect={selectedKey => this.switchTab(selectedKey)}>
+                        <Nav.Item>
+                            <Nav.Link  href="#" eventKey={ currentItem.Description }>Description</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey={ currentItem.Specs }>Specs</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey={ currentItem.Shipping }>Shipping</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey={ currentItem.Reviews }>Reviews</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Card.Body> 
+                <Card.Body>
+                    <Card.Text>
+                    { activeTabContent || currentItem.Description }
+                    </Card.Text>  
+                </Card.Body>    
+                <Button className="buy-btn">Buy Now</Button>         
+            </Card>
+           )
+        }
+        else {
+            return null;
+        }
     }
 
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import {InputGroup, FormControl }from 'react-bootstrap';
+import { InputGroup, FormControl }from 'react-bootstrap';
 import ResultsList from './ResultsList';
 
 
 class MainPage extends Component {
 
+    
+
     constructor(props) {
         super(props)
         this.state = {
             data: [],
+            filteredData: [],
             query: '',
-            loaded: false
+            loaded: false,
         }
     }
 
-    componentDidMount = () => {
-        const FETCH_URL = 'http://www.mocky.io/v2/5ccf3f8f300000ce1652c4bc';
+    componentDidMount = () => {       
+        const FETCH_URL = 'http://www.mocky.io/v2/5cd864f4300000a22a74cda3';
+
         fetch(FETCH_URL, {
-          method: 'GET'
+          method: 'GET',
         })
+
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+          console.log(json)
           this.setState({
             data: json,
             loaded: true
@@ -33,7 +38,12 @@ class MainPage extends Component {
     }
 
     handleSearchChange = (evt) => {
-        this.setState({ query: evt.target.value })
+        const { data, query } = this.state;
+        const filteredData = this.filterData(data, query);
+        this.setState({ 
+            query: evt.target.value, 
+            filteredData
+        })
     }
 
     filterData = (data, query) => {
@@ -42,13 +52,19 @@ class MainPage extends Component {
             return data.filter (
                 d => d.name.toLowerCase().indexOf(q) >-1
             )
-        }
-        
+        }      
     }
 
     render() {
-        const { data, query, loaded } = this.state;
-        const filteredData = this.filterData(data, query) || null;
+        const { data, filteredData, query, loaded } = this.state;
+        let presentedData;
+        if (loaded && query.length > 0) {
+            presentedData = <ResultsList listOfResults={ filteredData }/>;
+        } else if (loaded && !query.length > 0) {
+            presentedData = <ResultsList listOfResults={ data }/>;
+        } else {
+            presentedData = null;
+        }
         return (
             <div>               
                 <div className="searchContainer">
@@ -60,7 +76,7 @@ class MainPage extends Component {
                         />
                     </InputGroup>
                 </div>
-                {loaded ? <ResultsList listOfResults={ filteredData }/> : ''}
+                { presentedData }
             </div>
         )
     }
