241a40f14905e73c033385cfd82a0cb4e8fe6369
Stav Barak <stavb@tikalk.com>
2019-05-20 16:15:32 +0300

    token

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -13,13 +13,40 @@ export const NO_EPID = 'NO_EPID';
 const API_URL = 'https://api.ebay.com/buy/browse/v1/item_summary/search?category_ids=108765&filter=priceCurrency:USD&limit=45';
 
 const APP_TOKEN = 
-'v^1.1#i^1#I^3#f^0#p^1#r^0#t^H4sIAAAAAAAAAOVXbWwURRjuXe/aVChoaKwCMccq+Hl7u3vdvdsNd3qlRS5Cr3IFSpHg7N5sWbnb3ezM0Z4C1oIoCgoqFeIPasSi+JEIERFJjX/Ej5jGD0yMYiRBwB9KAoJoNDq7d5RrJUChCIm3Py77zjvvPM/zvu/MDtNRVnHbymkrf6t0lbu7O5gOt8vFjmAqyry3jyp1j/WWMEUOru6Omzo8naWHJyOQSZvSTIhMQ0fQ155J60hyjBEqa+mSAZCGJB1kIJKwIiVjM6ZLHM1IpmVgQzHSlC9eF6EUUWb4GhCsERlBrQmJxKqfitlkRCg5BHgR8kDmg6zAcZCMI5SFcR1hoOMIxTGs6Gd4Pys0sYLEchIv0KGQ2EL5ZkMLaYZOXGiGijpwJWeuVYT17FABQtDCJAgVjcemJhOxeF19Q9PkQFGsaEGHJAY4iwa+TTFS0DcbpLPw7Msgx1tKZhUFIkQFovkVBgaVYqfAXAB8R2oViGEhxIMwBEwqGFaGRcqphpUB+Ow4bIuW8quOqwR1rOHcuRQlasgPQAUX3hpIiHidz/67NwvSmqpBK0LV18bmxhobqSgRanEtsIAfZU2CFRv+xpl1flblVDnIy4pfCdWoaijFFRbKRyvIPGilKYae0mzRkK/BwLWQoIaDtWGKtCFOCT1hxVRsIyr2C5/SUAi32EnNZzGLF+p2XmGGCOFzXs+dgf7ZGFuanMWwP8LgAUeiCAVMU0tRgwedWiyUTzuKUAsxNqVAoK2tjW4L0obVGuAYhg00z5ieVBbCDKCIr93reX/t3BP8mkNFIW1K/CWcMwmWdlKrBIDeSkWDIhfmhILuA2FFB1v/ZSjiHBjYEcPWIaLCiSkeiAKvyDwAw9Eh0UKRBmwcUAY5fwZYiyA200CBfoXUWTYDLS0lBXmVC4ZV6E8JouqvEVXVL/MpgVQxhAyEsqyI4f9To5xvqScVw4SNRlpTcsNS8MNX7FaqEVg4l4TpNDGcb9WfkSSySV5yenavD4miHQORIMDUaLu2acXIBAxANjXbtMBBfVG8Y6YZz2SyGMhpGB+eDe0ybWZnpKeR4/6K4kTyl0+klsqf07STTRotVmgLIiNrkU8UOmEfW03GIqiTTQBbRjoNrdnsRSf6CsvvEPfKC+M9fAf1EHmTXucuaW0raY2U0ILLw+4yZ1UD+MpizfJ8mOGCbOjicjrFyWlT7j84i4ZEb5qBMExdgu/KwMBbbrTE+bGdrreZTtdb5KLMBJiJ7I3MhLLSWZ7SkWORhiGtAZVGWqtOLm8WpBfBnAk0y13m0p754rG9Rffq7vnMdf0364pSdkTRNZsZf3rEy46urmRFhmcF8nC80MLceHrUw17rqfqUul46vDrx0Cujm38ZVTWmfE1kyQ1MZb+Ty+Ut8XS6SjY05Goqd27ueqOvPPfeo5s9fS9unL8C3po4dM9G77Hv2p/fP/rjdvW+XeP8h/rG/34VW73pryPe1z86EH+wZ0F5qHemZ+P8pRWZcT1f7550YPLxviM/S3f0tva4gZx4Wt365JY50SPr5m3veuGPPYLngPvlleO+fxUALrR24paquVefnFC/eN9r25fjETu2dbz5TfOxD3tXrVpev3Vn09G7dqG/P6ne0b2kZ9Znc9Ynd7jfX7eBbl/x1PE1k6Yto6o+f6n22d4P3t3fAn76ds+mpXfqO49W7zk55sdb7vf1dWUPvpMbs+/mH3zrD9d6qceXndi97c+DI7/s+mrv1rvnbWtmlz28ln7kxDVd039d/cRzpfn0/QOOTqDK8RAAAA=='
-
+'v^1.1#i^1#f^0#p^1#r^0#I^3#t^H4sIAAAAAAAAAOVXW2wUVRjuttsqgWIKaKFcuky9xOLMntnd2e6M3cVtC2EF2souDVS0mZ050w6dnRnnnKEsqNn0oYSbFhMiIQSLmAgBU4kBTYyiD2K8BBMLDyTCqz6YeEsj0ZB4ZrqUbSVAYRES92Uz//nPf77v+///XECuYlr9wIqBPys9D5QO5UCu1ONhp4NpFeVLZpaV1pSXgAIHz1Du0Zy3v+ynRiRmNFNYA5Fp6Aj6Nmc0HQmuMUrZli4YIlKRoIsZiAQsCcn46lVCgAGCaRnYkAyN8iVaopTcIEscC1lO4cWGICcSq341ZsqIUlCUJT4cktISm5Z4PkzGEbJhQkdY1HGUCgCWpwFHB0CKDQosJwDANHCRTsrXAS2kGjpxYQAVc+EK7lyrAOuNoYoIQQuTIFQsEV+ebIsnWpa1phr9BbFieR2SWMQ2mvjVbMjQ1yFqNrzxMsj1FpK2JEGEKH9sbIWJQYX4VTC3Ad+VWgnzUqAhHGb5CB+SuKIoudywMiK+MQzHosq04roKUMcqzt5MUCJGeiOUcP6rlYRItPicv+dsUVMVFVpRallTfH28vZ2KEZ02NYmWSCPbJFixQbevaaFZJaCkg1xaoqWGkKI0yIH8QmPR8ipPWqnZ0GXV0Qz5Wg3cBAlqOFkbUKANcWrT26y4gh1EhX7cVQ1DfKeT07Ek2rhHd9IKM0QIn/t58wyMz8bYUtM2huMRJg+4EkUp0TRVmZo86JZivno2oyjVg7Ep+P19fX1MX5AxrG5/AADWv271qqTUAzOkF4mv0+uuv3rzCbTqUpEgmYlUAWdNgmUzKVUCQO+mYkE+EAmE87pPhBWbbP2XoYCzf2JDFKtBQlCBEuAACAQiIT4EitEhsXyR+h0cMC1m6Yxo9UJsaqIEaYnUmZ2BlioLQU4JBCMKpOUwr9AhXlHoNCeHSRVDCCBMk90v8n9qlFst9aRkmLDd0FQpW5yCL1qxW3K7aOFsEmoaMdxq1V+XJHJI3n16Tq9PhaITA5EgoqkyTm0zkpHxGyLZ1BxTl4v6jnjHTTORydhYTGswUaQN7d5sZtelp5LT/r7iRPI3lkhVHjunGTebDNokMRZEhm2RGwrT5hxbKaMX6mQTwJahadDqYO840fdZfqe4V94e7yIe1FPkTXqdvZu1LWkqKaGue8Tu3mZVFfH9xZrluEiQi7AA3BGvZjenqex/cRZNhd4KA2Eo34V7pX/iIzdW4v7Yfs9J0O85Qd7JwA8eY+vA4oqytd6yGTVIxZBRRYVBardO3m4WZHph1hRVq7TCo77x/bbzBc/qoRfA3PGH9bQydnrBKxssuDZSzj5UXcnygCOXpiBLrqedoO7aqJd9xDtn9sAuf+1B8+RwKvdebPilA7tHP0iAynEnj6e8xNvvKdnhO/L+jM+XJuYvebxny+XfYU9ncPj4h5X6aLUZOVX1w9CFtQ8fqG0Pb/3u7cZ5H7+ZW31u37mZn27665svuIWng0fqr/xxdMPrl3ceHL704Gn9113vHDp0/MzwMaM6i544A7+sL/duj649qB0Njox6E+cD+2d9u6fq3cOpuqdPXVpVt+PCJ6Vzy0eaB/Yu2/Yqtz5R1Vqzr8qaRS3Z+Yrvo9HPuue38euELV/3fHU6XP3L0rOzjz0/6Bu8sqdr5dLa1MW5P1dszJw4bMfbfxseXDTy4/bje+3dK5vqVzS+rNUs1mb0PjUivNVhLrS2GtPn7N9Q1vXia38/c3jBs/NqL8pnQ0n6ya1V+xZ0z1k0lr5/AHJWburwEAAA'
 const USER_TOKEN =
 'AgAAAA**AQAAAA**aAAAAA**6YzdXA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6AEkoupC5aHpgmdj6x9nY+seQ**ev4FAA**AAMAAA**Mflb0ZbULjU8u6S3Thqd70tloKTg+gVGAQKJnwbe7J9ycOmchtWmTpsrVaCJo7UUGUw3EqNdpHcbmc5Cz2FY/iedQZxzF8ePIiRD1YGlfkYzi0OrSwj8YUpZsq33FrG2UvqQGErbF0yhRcLfByuLGVjLExVUbBUEpNO0j+lvRqAc/7I61EjVGkLtw4Xbiw+u3Upy6p3NnYR1wgnL77AuB4d0a+TH0jTIbvJBSBV8SgBhJ4z8BmsTcP0ICo8TKZ49Te+W91ipks2Nr5M5OQagMxVCqIjtThvEm1HXt2WxzqePHWMF1DF5nfqYzFiyIv67VpdicJVuUqZkdTuqUUEoRECScYWsozpFoytjlVXJLrerr4aHITdmrdew+TctU2WbqlKnReS1sHA4WcsXduVTgcBXK65SytFhSd0hqcPIDD7eMKkxXlx69WT0WtlumBhTtud6prsl/Wn0iCbe8mY+EMCbEqZ/DpR8HYQwWvnzLRUhu4k4uUpK0owzMMoEQ+IiWjdlVkVKCglO9y/Umv4usFoxO9nmuxSWSWC1URM2uVEgN4+CdyH5ZjBWjcmbo/C1vZAx3o42Xl9cISAsOSPnpktOIPL3cWdlk4tj6ZV22SuezutX8GDb8ka80mAj10dxdaX4zAt4GgYi5QddN4RiG9bDirddNuXIxItnq31k7gwvmV9j3ZtqmJEHsst2MR/CcyxOOE+7a5oMplqyk9YbHNxJCeoDsvG8GDvEdoXLSekxO4u+j3rdKCNECVmE1fAw'
 
 const APP_AUTH = `Bearer ${APP_TOKEN}`;
 const USER_AUTH = `Bearer ${USER_TOKEN}`;
+
+
+const CLIENT_ID = 'StavBara-supersto-SBX-aeaa3d7e6-66b66990';
+const CLIENT_SECRET = 'SBX-eaa3d7e660d8-0812-4dd7-99b0-442e';
+
+/* export function refreshToken() {
+  return fetch('https://api.ebay.com/identity/v1/oauth2/token', {
+    method: 'post',
+    headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+    },
+    body: qs.stringify({
+    grant_type: 'authorization_code',
+    // parsed from redirect URI after returning from eBay,
+    code: authCode,
+    // this is set in your dev account, also called RuName
+    redirect_uri: '5711ece8-470b-422f-8166-a547c975c753'
+    })
+   })
+   .then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  )
+  .then(json =>
+    console.log(json)
+  )
+} */
 
 export async function fetchData(dispatch) {
     return await fetch(API_URL, {
